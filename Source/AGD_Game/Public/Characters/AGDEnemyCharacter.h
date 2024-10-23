a744e69405f9c38ae8ab19970332b6ce1b1eef36
10241AdamW <10241@ait.nsw.edu.au>
2024-10-24 01:23:02 +1100

    Module 6 - Enemy Combat

--- Source/AGD_Game/Public/Characters/AGDEnemyCharacter.h
+++ Source/AGD_Game/Public/Characters/AGDEnemyCharacter.h
@@ -10,5 +10,19 @@ UCLASS()
 class AGD_GAME_API AAGDEnemyCharacter : public AAGDCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+	AAGDEnemyCharacter() { AttackRange = 200.0f; }
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Alternate Stats")
+	float AttackRange;
+
+public:
+	UFUNCTION(BlueprintPure, Category = "Alternate Stats")
+	float GetAttackRange() { return AttackRange; }
+
+	UFUNCTION(BlueprintCallable, Category = "Alternate Stats")
+	void SetAttackRange(float NewRange) { AttackRange = NewRange; }
+
 };
