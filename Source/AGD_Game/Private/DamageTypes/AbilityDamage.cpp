8e0cc976e7cb756790144950f00e9f1111d570bf
10241AdamW <10241@ait.nsw.edu.au>
2024-11-19 01:49:36 +1100

    Dragon Flight + More Anims

---
+++ Source/AGD_Game/Private/DamageTypes/AbilityDamage.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DamageTypes/AbilityDamage.h"
+
+UAbilityDamage::UAbilityDamage()
+{
+	// Could add a damage multiplier to be processed in c++
+}
---
+++ Source/AGD_Game/Public/DamageTypes/AbilityDamage.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "AbilityDamage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AGD_GAME_API UAbilityDamage : public UDamageType
+{
+	GENERATED_BODY()
+	
+public:
+	UAbilityDamage();
+};
